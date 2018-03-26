44bfe90a4b7542a7b8722fa2e12a58940cb6dae7
Jennifer Reif <jennifer.reif@neo4j.com>
2018-03-26 08:20:42 -0500

    Cleaned up unneeded dependency and annotation.

--- src/main/java/movies/spring/data/neo4j/repositories/MovieRepository.java
+++ src/main/java/movies/spring/data/neo4j/repositories/MovieRepository.java
@@ -6,13 +6,11 @@ import movies.spring.data.neo4j.domain.Movie;
 import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 /**
  * @author Michael Hunger
  * @author Mark Angrish
  */
-@Repository
 public interface MovieRepository extends Neo4jRepository<Movie, Long> {
 
     @Query("MATCH (m:Movie {title: {title}}) RETURN m LIMIT 1")
