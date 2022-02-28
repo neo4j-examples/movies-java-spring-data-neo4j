787660e3883386558a3dfa9ce9ab50e5375174bb
Aaman Lamba <aamanlamba@gmail.com>
2022-02-28 15:52:37 -0500

    Graph Catalog initial commit

---
+++ src/main/java/movies/spring/data/neo4j/api/SchemaController.java
@@ -0,0 +1,9 @@
+package movies.spring.data.neo4j.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SchemaController {
+    private final ArrayList<String> nodeLabels;
+    
+}
